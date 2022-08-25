4835fd37e66d0e408a4023cd9a58508aa9691a66
simran <95364621+Simran8800@users.noreply.github.com>
2022-08-25 14:53:45 +0530

    Update Trangle with same no.py

--- Trangle with same no.py
+++ Trangle with same no.py
@@ -1,11 +1,11 @@
-# i=1
-# while i<=6:
-#     j=1
-#     while j<=i:
-#         print(i,end="")
-#         j=j+1
-#     print()
-#     i=i+1
+i=1
+while i<=6:
+    j=1
+    while j<=i:
+        print(i,end="")
+        j=j+1
+    print()
+    i=i+1
 
 
 
